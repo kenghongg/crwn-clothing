e742ddff13906737c0282fca735bcb598675b847
Keng Hong Ho <kenghongho@Kengs-MacBook-Pro.local>
2020-06-04 00:07:38 +0800

    finishing sign in handleSubmit method

--- src/App.js
+++ src/App.js
@@ -31,6 +31,8 @@ class App extends React.Component {
               id: snapShot.id,
               ...snapShot.data()
             }
+          }, () => {
+            console.log(this.state);
           });
         });
       } else {
